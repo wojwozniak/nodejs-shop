d0ee848352f53e0203bd8b64c6d0ecab841a6a90
Wojtek <pulchnymamut@gmail.com>
2023-12-21 18:50:45 +0100

    connected to mongoDb

---
+++ server.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const creditentials = require('./creditentials.json');
+
+const uri = `mongodb+srv://${creditentials.databaseUser}:${creditentials.databasePassword}@weppo.sew572t.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
