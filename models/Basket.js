6000727deb479963be236b9c8bd12bbbd0b4c384
Wojtek <pulchnymamut@gmail.com>
2023-12-27 20:31:44 +0100

    do not store user pointer in basket, one way is enough

--- models/Basket.js
+++ models/Basket.js
@@ -4,8 +4,7 @@ const BasketSchema = new mongoose.Schema({
     items: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
         quantity: Number
-    }],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }]
 });
 
 const Basket = mongoose.model('Basket', BasketSchema);
