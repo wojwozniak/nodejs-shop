ad24b5742122595cf15466f7c284970a9b70a922
Wojtek <pulchnymamut@gmail.com>
2023-12-27 22:35:50 +0100

    order model

---
+++ models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  items: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    quantity: Number
+  }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  date: Date
+});
+
+const Order = mongoose.model('Product', ProductSchema);
+
+module.exports = Order;
